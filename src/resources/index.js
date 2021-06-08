1c63125002541e8fb481815ae6437dad2dc25136
hansa0712004212 <hansa0712004212@gmail.com>
2021-06-08 14:35:09 +0530

    Sky plugin added

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,2 +1,41 @@
 export const IMAGE_LOGO = require("./images/logo.png");
 export const IMAGE_CHANGE = require("./images/change.png");
+export const IMAGE_LOVE_1 = require("./images/love_1.png");
+export const IMAGE_LOVE_2 = require("./images/love_2.png");
+export const IMAGE_CLOUD_1 = require("./images/cloud_1.png");
+export const IMAGE_CLOUD_2 = require("./images/cloud_2.png");
+export const IMAGE_WESAK_1 = require("./images/wesak_1.png");
+export const IMAGE_WESAK_2 = require("./images/wesak_2.png");
+export const IMAGE_WESAK_3 = require("./images/wesak_3.png");
+export const IMAGE_CHRISTMAS_1 = require("./images/christmas_1.png");
+export const IMAGE_CHRISTMAS_2 = require("./images/christmas_2.png");
+export const IMAGE_CHRISTMAS_3 = require("./images/christmas_3.png");
+export const IMAGE_CHRISTMAS_4 = require("./images/christmas_4.png");
+export const IMAGE_CHRISTMAS_5 = require("./images/christmas_5.png");
+
+export const WALLPAPER_SKY_1 = require("./images/wallpaper1.jpg");
+export const WALLPAPER_SKY_2 = require("./images/wallpaper2.jpg");
+
+export const IMAGE_LOVE_SET = {
+  0: IMAGE_LOVE_1.default,
+  1: IMAGE_LOVE_2.default
+}
+
+export const IMAGE_CLOUD_SET = {
+  0: IMAGE_CLOUD_1.default,
+  1: IMAGE_CLOUD_2.default
+}
+
+export const IMAGE_WESAK_SET = {
+  0: IMAGE_WESAK_1.default,
+  1: IMAGE_WESAK_2.default,
+  2: IMAGE_WESAK_3.default
+}
+
+export const IMAGE_CHRISTMAS_SET = {
+  0: IMAGE_CHRISTMAS_1.default,
+  1: IMAGE_CHRISTMAS_2.default,
+  2: IMAGE_CHRISTMAS_3.default,
+  3: IMAGE_CHRISTMAS_4.default,
+  4: IMAGE_CHRISTMAS_5.default
+}
--- src/ui/App.js
+++ src/ui/App.js
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AudioSpectrum from "react-audio-spectrum";
+import Sky from "react-sky";
 import RkImage from "../components/RkImage";
 import { MeterColors, Strings, Urls } from "../constants";
-import { IMAGE_CHANGE, IMAGE_LOGO } from "../resources";
+import { IMAGE_CHANGE, IMAGE_CLOUD_SET, IMAGE_LOGO, IMAGE_LOVE_SET, IMAGE_WESAK_SET, WALLPAPER_SKY_2 } from "../resources";
 import "./App.css";
 
-function App() {
+const getWelcomeText = () => {
+  let text = "";
+  const hour = new Date().getHours();
+  if (hour < 12) {
+    text = "Good Morning";
+  } else if (hour < 14) {
+    text = "Good Afternoon";
+  } else {
+    text = "Good Evening";
+  }
+  return text;
+};
+
+const getImageSet = () => {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  let imageSet = IMAGE_CLOUD_SET;
+  if ((month === 5 && [26, 27].includes(day)) || (month === 6 && day === 24)) {
+    imageSet = IMAGE_WESAK_SET;
+  } else if ((month === 2 && day === 14)) {
+    imageSet = IMAGE_LOVE_SET;
+  }
+  return imageSet;
+};
+
+const App = () => {
   const width = window.innerWidth;
   const height = window.innerHeight;
   const [spectrumIndex, setSpectrumIndex] = useState(0);
-  const [mediaButtonText, setMediaButtonText] = useState(Strings.PAUSE);
+  const [mediaButtonText, setMediaButtonText] = useState(Strings.PLAY);
   const [meterColor, setMeterColor] = useState(MeterColors.RAINBOW);
-  const [isPlaying, setIsPlaying] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [welcome, setWelcome] = useState(getWelcomeText());
+  const [imageSet] = useState(getImageSet());
+
+  useEffect(() => {
+    setInterval(() => setWelcome(getWelcomeText()), 60000);
+  });
 
   const play = (event) => {
     event.preventDefault();
@@ -41,38 +74,46 @@ function App() {
     player.play();
     let mediaButton = document.getElementById("media-button");
     mediaButton.focus();
-  }
+  };
 
   return (
-    <div style={{ display: "flex", flex: 1, flexDirection: "column", justifyContent: "space-between", alignItems: "center", maxHeight: height, height: height }}>
-      <RkImage source={IMAGE_LOGO} width={200} height={200} style={{ marginTop: 20 }} />
-      <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center" }}>
-        <button className={`media-button ${isPlaying ? 'heart-bit-neon' : 'heart-bit-neon-inactive'}`} onClick={play} id="media-button">
-          {mediaButtonText}
-        </button>
-        <audio controls id="audio-element" autoPlay={true} crossOrigin="anonymous" preload="auto">
-          <source src={Urls.PRIMARY_STREAM} type="audio/mpeg" />
-          <source src={Urls.PRIMARY_STREAM} type="audio/mp3" />
-        </audio>
-        {isPlaying &&
-          <button className={"change-button"} onClick={changeSpectrum}>
-            <RkImage source={IMAGE_CHANGE} width={64} height={64} />
+    <div style={{ opacity: 0.9, backgroundImage: `url(${WALLPAPER_SKY_2.default})`, backgroundRepeat: "no-repeat", display: "flex", flex: 1, flexDirection: "column", justifyContent: "space-between", alignItems: "center", maxHeight: height, height: height }}>
+      <div style={{ position: "relative", display: "flex", flex: 1, flexDirection: "column", justifyContent: "space-between", alignItems: "center", maxHeight: height, height: height }}>
+        <p id="welcome">{welcome}</p>
+        <RkImage source={IMAGE_LOGO} width={200} height={200} />
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center" }}>
+          <audio controls id="audio-element" autoPlay={false} crossOrigin="anonymous" preload="auto">
+            <source src={Urls.PRIMARY_STREAM} type="audio/mpeg" />
+            <source src={Urls.PRIMARY_STREAM} type="audio/mp3" />
+          </audio>
+          <button className={`media-button ${isPlaying ? "heart-bit-neon" : "heart-bit-neon-inactive"}`} onClick={play} id="media-button">
+            {mediaButtonText}
           </button>
-        }
+          <button className={`${isPlaying ? '' : 'change-button-hide'} change-button`} onClick={changeSpectrum}>
+            <RkImage source={IMAGE_CHANGE} width={32} height={32} />
+          </button>
+        </div>
+        <br />
+        <AudioSpectrum
+          id="audio-canvas"
+          crossOrigin="anonymous"
+          height={200}
+          width={width}
+          audioId={"audio-element"}
+          capColor={meterColor[meterColor.length - 1].color}
+          capHeight={2}
+          meterWidth={6}
+          meterCount={512}
+          meterColor={meterColor}
+          gap={2} />
       </div>
-      <br />
-      <AudioSpectrum
-        id="audio-canvas"
-        crossOrigin="anonymous"
-        height={200}
-        width={width}
-        audioId={"audio-element"}
-        capColor={meterColor[meterColor.length - 1].color}
-        capHeight={2}
-        meterWidth={6}
-        meterCount={512}
-        meterColor={meterColor}
-        gap={2} />
+      <Sky
+        images={imageSet}
+        how={30} /* Pass the number of images Sky will render chosing randomly */
+        time={60} /* time of animation */
+        size={"100px"} /* size of the rendered images */
+        background={`url(${WALLPAPER_SKY_2.default})`} /* color of background */
+      />
     </div>
   );
 }
