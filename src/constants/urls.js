7fba1e4c85ad88bdc3757002b13dc488982d1fdd
hansa0712004212 <hansa0712004212@gmail.com>
2021-06-09 23:35:41 +0530

    makes a mixed content https to http call which is not accessible

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,3 @@
-const PRIMARY_URL = "http://66.70.245.224:8016";
+const PRIMARY_URL = "//66.70.245.224:8016";
 export const PRIMARY_STREAM = `${PRIMARY_URL}/stream.mp3?sid=1`; //"http://66.70.245.224:8016/;stream#.mp3?_=1";
 export const CURRET_TRACK = `${PRIMARY_URL}/currentsong?sid=1`;
